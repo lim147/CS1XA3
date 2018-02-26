da5ab47e1c7dc80fc4b3e1daee69fbcc45bdad9a
Meijing Li <lim147@mills.cas.mcmaster.ca>
2018-02-26 00:55:47 -0500

    add modified Assignment 1

--- ProjectAnalyze.sh
+++ ProjectAnalyze.sh
@@ -14,10 +14,11 @@ fi
 #2.Put all uncommited changes in a file changes.log
 git diff --staged > changes.log #method 1
 git diff  > changes.log         #method 2
-
+echo "uncommited changes put in changes.log"
 
 #3.Put each line from every file of your project with the tag #TODO into a file todo.log
 grep -r "#TODO" --exclude todo.log . > todo.log
+echo "lines with #TODO put in todo.log"
 
 #4.Check all haskell files for syntax errors and puts the results into error.log
 shopt -s nullglob
@@ -27,4 +28,4 @@ find . -name "*.hs" |
      do
          ghc -fno-code $f 2>> error.log
      done
-
+echo "haskell syntax errors put in error.log"
